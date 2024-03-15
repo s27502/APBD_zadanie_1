2e11daaa01f7c59882ad085b0822bdfcc66b093e
Jakub Rychta <s27502@pjwstk.edu.pl>
2024-03-15 09:47:46 +0100

    modified feature-max

--- apbd/apbd/Program.cs
+++ apbd/apbd/Program.cs
@@ -6,10 +6,20 @@ public class Program
     {
         int[] arr = { 1, 2, 3, 4, 5 };
         Console.WriteLine(MaxVal(arr));
+        Console.WriteLine(avg(arr));
     }
 
     public static int MaxVal(int[] arr)
     {
         return arr.Max();
     } 
+    public static double avg(int[] arr)
+    {
+        double result = 0.0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            result += arr[i];
+        }
+        return result/arr.Length;
+    }
 }
