c425a8d5516c692dbc9eb8e19cb24c401a9c2f40
s27502 <s27502@pjwstk.edu.pl>
2024-03-16 11:32:39 +0100

    babadeeba

--- apbd/apbd/Program.cs
+++ apbd/apbd/Program.cs
@@ -5,12 +5,18 @@ public class Program
     public static void Main(string[] args)
     {
         int[] arr = { 1, 2, 3, 4, 5 };
-        Console.WriteLine(MaxVal(arr));
+        Console.WriteLine(avg(arr));
     }
 
-    public static int MaxVal(int[] arr)
+    public static double avg(int[] arr)
     {
-        return arr.Max();
-    } 
+        double result = 0.0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            result += arr[i];
+            Console.WriteLine("abc");
+        }
+        return result/arr.Length;
+    }
     
 }
